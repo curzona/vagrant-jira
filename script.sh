09d65f4946780138158cd78cce82873d0c4f2afd
curzona <curzona@gmail.com>
2015-06-06 14:00:58 -0700

    First commit

---
+++ script.sh
@@ -0,0 +1,6 @@
+apt-get -y update
+apt-get -y install vim curl nmap tree zip htop upstart 
+apt-get -y install openjdk-7-jre-headless
+
+wget https://downloads.atlassian.com/software/jira/downloads/atlassian-jira-6.4.5-x64.bin
+chmod a+x atlassian-jira-6.4.5-x64.bin
